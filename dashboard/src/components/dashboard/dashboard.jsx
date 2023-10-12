070afb2bdfba5995fbbab4550ac097b9b69f2645
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-13 01:28:33 +0530

    Add: margin and fix width

--- dashboard/src/components/dashboard/dashboard.jsx
+++ dashboard/src/components/dashboard/dashboard.jsx
@@ -79,9 +79,10 @@ const Dashboard = () => {
           component="form"
           sx={{
             p: "2px",
+            margin: "0px 0px 0px 2rem", // rem to provide responsiveness
             display: "flex",
             alignItems: "center",
-            width: "20%", // Updated width for responsiveness
+            width: "10%", // Updated width for responsiveness
             background: "black",
             borderRadius: "25px",
             fontFamily: "Poppins, sans-serif",
